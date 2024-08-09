5b798b540aa629220c5c32eb868b393e1a95f7e3
GamingHazard <bb466882@gmail.com>
2024-08-09 13:40:34 -0700

    updated commit

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const ws = require("ws");
-
 require("dotenv").config();
 
 const app = express();
@@ -30,21 +27,24 @@ mongoose
     console.log("Error Connecting to MongoDB");
   });
 
-app.listen(port, () => {
-  console.log("server is running on port 3000");
+// Create the HTTP server
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-// Set up WebSocket Server
+// Create the WebSocket server
 const wss = new ws.Server({ server });
 
-wss.on("connection", (socket) => {
+// WebSocket connection handling
+wss.on("connection", (ws) => {
   console.log("Client connected");
 
-  socket.on("message", (message) => {
+  ws.on("message", (message) => {
     console.log("Received:", message);
+    // Handle incoming messages and broadcast them if necessary
   });
 
-  socket.on("close", () => {
+  ws.on("close", () => {
     console.log("Client disconnected");
   });
 });
@@ -54,7 +54,7 @@ const Post = require("./models/post");
 const salesPost = require("./models/sellPost");
 const buyPost = require("./models/buyPost");
 
-//endpoint to register a user in the backend
+// Endpoint to register a user
 app.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -64,55 +64,45 @@ app.post("/register", async (req, res) => {
       return res.status(400).json({ message: "Email already registered" });
     }
 
-    //create a new user
     const newUser = new User({ name, email, password });
-
-    //generate and store the verification token
     newUser.verificationToken = crypto.randomBytes(20).toString("hex");
 
-    //save the  user to the database
     await newUser.save();
-
-    //send the verification email to the user
     sendVerificationEmail(newUser.email, newUser.verificationToken);
 
     res.status(200).json({ message: "Registration successful" });
   } catch (error) {
-    console.log("error registering user", error);
-    res.status(500).json({ message: "error registering user" });
+    console.log("Error registering user", error);
+    res.status(500).json({ message: "Error registering user" });
   }
 });
 
 const sendVerificationEmail = async (email, verificationToken) => {
-  //create a nodemailer transporter
-
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "",
-      pass: "",
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
-  //compose the email message
   const mailOptions = {
     from: "Uga-Cycle",
     to: email,
     subject: "Email Verification",
-    text: `please click the following link to verify your email https://waste-recycle-app-backend.onrender.com/${verificationToken}`,
+    text: `Please click the following link to verify your email: https://waste-recycle-app-backend.onrender.com/verify/${verificationToken}`,
   };
 
   try {
     await transporter.sendMail(mailOptions);
   } catch (error) {
-    console.log("error sending email", error);
+    console.log("Error sending email", error);
   }
 };
 
 app.get("/verify/:token", async (req, res) => {
   try {
     const token = req.params.token;
-
     const user = await User.findOne({ verificationToken: token });
     if (!user) {
       return res.status(404).json({ message: "Invalid token" });
@@ -124,14 +114,13 @@ app.get("/verify/:token", async (req, res) => {
 
     res.status(200).json({ message: "Email verified successfully" });
   } catch (error) {
-    console.log("error getting token", error);
+    console.log("Error verifying token", error);
     res.status(500).json({ message: "Email verification failed" });
   }
 });
 
 const generateSecretKey = () => {
-  const secretKey = crypto.randomBytes(32).toString("hex");
-  return secretKey;
+  return crypto.randomBytes(32).toString("hex");
 };
 
 const secretKey = generateSecretKey();
@@ -139,7 +128,6 @@ const secretKey = generateSecretKey();
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "Invalid email" });
@@ -150,32 +138,30 @@ app.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, secretKey);
-
     res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ message: "Login failed" });
   }
 });
 
-//endpoint to access all the users except the logged in the user
+// Endpoint to get all users except the logged-in user
 app.get("/user/:userId", (req, res) => {
   try {
     const loggedInUserId = req.params.userId;
-
     User.find({ _id: { $ne: loggedInUserId } })
       .then((users) => {
         res.status(200).json(users);
       })
       .catch((error) => {
-        console.log("Error: ", error);
-        res.status(500).json("errror");
+        console.log("Error:", error);
+        res.status(500).json({ message: "Error getting users" });
       });
   } catch (error) {
-    res.status(500).json({ message: "error getting the users" });
+    res.status(500).json({ message: "Error getting users" });
   }
 });
 
-//endpoint to follow a particular user
+// Endpoint to follow a user
 app.post("/follow", async (req, res) => {
   const { currentUserId, selectedUserId } = req.body;
 
@@ -187,11 +173,11 @@ app.post("/follow", async (req, res) => {
     res.sendStatus(200);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "error in following a user" });
+    res.status(500).json({ message: "Error following user" });
   }
 });
 
-//endpoint to unfollow a user
+// Endpoint to unfollow a user
 app.post("/users/unfollow", async (req, res) => {
   const { loggedInUserId, targetUserId } = req.body;
 
@@ -206,90 +192,14 @@ app.post("/users/unfollow", async (req, res) => {
   }
 });
 
-// //endpoint to create a new post in the backend
-// app.post("/create-post", async (req, res) => {
-//   try {
-//     const { content, userId } = req.body;
-
-//     const newPostData = {
-//       user: userId,
-//     };
-
-//     if (content) {
-//       newPostData.content = content;
-//     }
-
-//     const newPost = new Post(newPostData);
-
-//     await newPost.save();
-
-//     res.status(200).json({ message: "Post saved successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "post creation failed" });
-//   }
-// });
-// //endpoint to create a new sales post in the backend
-// app.post("/create-SalePosts", async (req, res) => {
-//   try {
-//     const { content, userId } = req.body;
-
-//     const newSalesPostData = {
-//       user: userId,
-//     };
-
-//     if (content) {
-//       newSalesPostData.content = content;
-//     }
-
-//     const salepost = new salesPost(newSalesPostData);
-
-//     await salepost.save();
-
-//     res.status(200).json({ message: "Post saved successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "post creation failed" });
-//   }
-// });
-// //endpoint to create a new buy post in the backend
-// app.post("/create-BuyPosts", async (req, res) => {
-//   try {
-//     const { content, userId } = req.body;
-
-//     const newBuyPostData = {
-//       user: userId,
-//     };
-
-//     if (content) {
-//       newBuyPostData.content = content;
-//     }
-
-//     const newBuyPost = new buyPost(newBuyPostData);
-
-//     await newBuyPost.save();
-
-//     res.status(200).json({ message: "Post saved successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "post creation failed" });
-//   }
-// });
-
-// Example WebSocket route to notify clients when a new post is created
+// Endpoint to create a new post
 app.post("/create-post", async (req, res) => {
   try {
     const { content, userId } = req.body;
 
-    const newPostData = {
-      user: userId,
-    };
-
-    if (content) {
-      newPostData.content = content;
-    }
-
-    const newPost = new Post(newPostData);
+    const newPost = new Post({ user: userId, content });
     await newPost.save();
 
-    // Notify all connected clients about the new post
     wss.clients.forEach((client) => {
       if (client.readyState === ws.OPEN) {
         client.send(JSON.stringify({ type: "NEW_POST", post: newPost }));
@@ -302,23 +212,14 @@ app.post("/create-post", async (req, res) => {
   }
 });
 
-// Example WebSocket route to notify clients when a new sales post is created
+// Endpoint to create a new sales post
 app.post("/create-SalePosts", async (req, res) => {
   try {
     const { content, userId } = req.body;
 
-    const newSalesPostData = {
-      user: userId,
-    };
-
-    if (content) {
-      newSalesPostData.content = content;
-    }
-
-    const salepost = new salesPost(newSalesPostData);
+    const salepost = new salesPost({ user: userId, content });
     await salepost.save();
 
-    // Notify all connected clients about the new sales post
     wss.clients.forEach((client) => {
       if (client.readyState === ws.OPEN) {
         client.send(JSON.stringify({ type: "NEW_SALES_POST", post: salepost }));
@@ -331,23 +232,14 @@ app.post("/create-SalePosts", async (req, res) => {
   }
 });
 
-// Example WebSocket route to notify clients when a new buy post is created
+// Endpoint to create a new buy post
 app.post("/create-BuyPosts", async (req, res) => {
   try {
     const { content, userId } = req.body;
 
-    const newBuyPostData = {
-      user: userId,
-    };
-
-    if (content) {
-      newBuyPostData.content = content;
-    }
-
-    const newBuyPost = new buyPost(newBuyPostData);
+    const newBuyPost = new buyPost({ user: userId, content });
     await newBuyPost.save();
 
-    // Notify all connected clients about the new buy post
     wss.clients.forEach((client) => {
       if (client.readyState === ws.OPEN) {
         client.send(JSON.stringify({ type: "NEW_BUY_POST", post: newBuyPost }));
@@ -360,18 +252,18 @@ app.post("/create-BuyPosts", async (req, res) => {
   }
 });
 
-//endpoint for liking a particular post
+// Endpoint for liking a post
 app.put("/posts/:postId/:userId/like", async (req, res) => {
   const postId = req.params.postId;
-  const userId = req.params.userId; // Assuming you have a way to get the logged-in user's ID
+  const userId = req.params.userId;
 
   try {
     const post = await Post.findById(postId).populate("user", "name");
 
     const updatedPost = await Post.findByIdAndUpdate(
       postId,
-      { $addToSet: { likes: userId } }, // Add user's ID to the likes array
-      { new: true } // To return the updated post
+      { $addToSet: { likes: userId } },
+      { new: true }
     );
 
     if (!updatedPost) {
@@ -388,7 +280,7 @@ app.put("/posts/:postId/:userId/like", async (req, res) => {
   }
 });
 
-//endpoint to unlike a post
+// Endpoint to unlike a post
 app.put("/posts/:postId/:userId/unlike", async (req, res) => {
   const postId = req.params.postId;
   const userId = req.params.userId;
@@ -417,7 +309,7 @@ app.put("/posts/:postId/:userId/unlike", async (req, res) => {
   }
 });
 
-//endpoint to get all the posts
+// Endpoint to get all posts
 app.get("/get-posts", async (req, res) => {
   try {
     const posts = await Post.find()
@@ -431,7 +323,8 @@ app.get("/get-posts", async (req, res) => {
       .json({ message: "An error occurred while getting the posts" });
   }
 });
-//endpoint to get all the  Buy posts
+
+// Endpoint to get all Buy posts
 app.get("/get-BuyPosts", async (req, res) => {
   try {
     const BuyPosts = await buyPost
@@ -443,10 +336,11 @@ app.get("/get-BuyPosts", async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .json({ message: "An error occurred while getting the posts" });
+      .json({ message: "An error occurred while getting the Buy posts" });
   }
 });
-//endpoint to get all the  Sale posts
+
+// Endpoint to get all Sale posts
 app.get("/get-SalePosts", async (req, res) => {
   try {
     const SalePosts = await salesPost
@@ -458,10 +352,11 @@ app.get("/get-SalePosts", async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .json({ message: "An error occurred while getting the posts" });
+      .json({ message: "An error occurred while getting the Sale posts" });
   }
 });
 
+// Endpoint to get user profile
 app.get("/profile/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
